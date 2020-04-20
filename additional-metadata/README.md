a2a703b086bd6aa89180fb9c782a1cf210c50af3
Luke Butters <lbutters@funnelback.com>
2020-04-21 09:37:28 +1000

    Improve the metadata mappings example

--- additional-metadata/README.md
+++ additional-metadata/README.md
@@ -1,27 +1,35 @@
 # Plugin: additional-metadata
 
-This example explains how we can use the `Indexing` plugin to add some additional metadata
-The additional metadata value must be provided as a `collection.cfg` parameter.
+Demonstrates using a plugin to supply additional metadata mappings.
+
+Also shows a plugin reading from collection.cfg, in this case to change the metadata 
+mappings supplied.
 
 ## Usage
 
-Add the following to your `collection.cfg` to enable the plugin.
+Add the following to `collection.cfg` to enable the plugin.
 
 ```ini
 plugin.additional-metadata.enabled=true
-plugin.additional-metadata.version=1.0
+plugin.additional-metadata=1.0
 ```
 
-Add the following to your `collection.cfg` to define the regex pattern:
+The following `collection.cfg` settings can be used to configure the plugin:
+
+* `plugin.additional-metadata.metadata`: Defines an additional HTML <meta> 'name' to map to the 'authors' metadata class.
+
+
+For example to also map the content of HTML meta tags with `name='publishers'` to metadata class authors add to collection.cfg:
+
 
 ```ini
-plugin.additional-metadata.metadata=<STRING>
+plugin.additional-metadata.metadata=publishers
 ```
 
-e.g.
+which will result in the content of tags like:
 
-```ini
-plugin.additional-metadata.metadata=author
+```
+<meta name='publishers' content="...">
 ```
 
-will add `author` metadata.
+being mapped and indexed into metadata class 'authors'. This will be available to the query processor.
--- additional-metadata/src/test/java/com/example/AdditionalMetadataIndexingConfigProviderTest.java
+++ additional-metadata/src/test/java/com/example/AdditionalMetadataIndexingConfigProviderTest.java
@@ -13,25 +13,29 @@ public class AdditionalMetadataIndexingConfigProviderTest {
 
     @Test
     public void testMetadataMappings(){
-        AdditionalMetadataIndexingConfigProvider underTest = new AdditionalMetadataIndexingConfigProvider();
         MockIndexConfigProviderContext mockContext = new MockIndexConfigProviderContext();
         MockMetadataMappingConsumer mockConsumer = new MockMetadataMappingConsumer();
+        AdditionalMetadataIndexingConfigProvider underTest = new AdditionalMetadataIndexingConfigProvider();
+        
+        // Set collection.cfg config options.
+        mockContext.setConfigSetting("plugin.additional-metadata.metadata", "publishers");
 
-        mockContext.setCollectionName("collection");
-        mockContext.setConfigSetting("plugin.additional-metadata.metadata", "FUNkgLiveUrl");
-
+        // Now run the method on our class we implemented.
         underTest.metadataMappings(mockContext, mockConsumer);
 
+        // This list holds the result of what the plugin class.
         List<MockMetadataMappingConsumer.MockMetadataMappingInvocation> invocations = mockConsumer.getInvocations();
 
-        Assert.assertEquals(2, invocations.size());
-        Assert.assertEquals("author", invocations.get(0).getMetadataClass());
+        Assert.assertEquals("Expected to see the plugin provide two metadata mappings.", 2, invocations.size());
+        
+        // Now check each of the invocations ensuring that the correct values were set.
+        Assert.assertEquals("authors", invocations.get(0).getMetadataClass());
         Assert.assertEquals("author", invocations.get(0).getLocator());
         Assert.assertEquals(MetadataSourceType.HTML_OR_HTTP_HEADERS, invocations.get(0).getSourceType());
         Assert.assertEquals(MetadataType.TEXT_NOT_INDEXED_AS_DOCUMENT_CONTENT, invocations.get(0).getType());
 
-        Assert.assertEquals("FUNkgLiveUrl", invocations.get(1).getMetadataClass());
-        Assert.assertEquals("FUNkgLiveUrl", invocations.get(1).getLocator());
+        Assert.assertEquals("authors", invocations.get(1).getMetadataClass());
+        Assert.assertEquals("In this case we should have read 'publishers' from config settings.", "publishers", invocations.get(1).getLocator());
         Assert.assertEquals(MetadataSourceType.HTML_OR_HTTP_HEADERS, invocations.get(1).getSourceType());
         Assert.assertEquals(MetadataType.TEXT_INDEXED_AS_DOCUMENT_CONTENT, invocations.get(1).getType());
     }
